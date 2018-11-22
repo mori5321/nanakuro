5487f5f45d04501628320b7288891aaacc04725e
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-11-22 16:02:24 +0900

    introduce nuxt-js

---
+++ nanakuro-client/pages/question_groups/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <section>
+    GoodNight
+    <el-input placeholder="Please input" />
+  </section>
+</template>
---
+++ nanakuro-client/pages/questions/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <section>
+    Hello
+  </section>
+</template>
